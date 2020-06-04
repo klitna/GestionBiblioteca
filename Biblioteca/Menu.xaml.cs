64fea88cd25ac4ab68135ed46f1b53626b0404fc
Iryna Klitna <cf19iryna.klitna@itequia.com>
2020-06-04 12:19:16 +0200

    toolbar

---
+++ Biblioteca/Menu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Biblioteca
+{
+    public partial class Menu : ContentPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
