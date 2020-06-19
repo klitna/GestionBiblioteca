63f05e508497a634aa34baea94f0d88daefc23ee
iryna.klitna <iryna.klitna@itequia.com>
2020-06-19 04:56:50 +0200

    bd

--- Biblioteca/Registration.xaml.cs
+++ Biblioteca/Registration.xaml.cs
@@ -6,7 +6,24 @@ using Xamarin.Forms;
 namespace Biblioteca
 {
     public partial class MyPage : ContentPage
+
     {
+        public System.Windows.Input.ICommand RegisterCommand => new Command(Register);
+
+        public void Register()
+        {
+            //Comprobar en bd que no exista el login
+            /*
+             * var saveButton = new Button { Text = "Save" };
+                saveButton.Clicked += async (sender, e) =>
+                {
+                    var todoItem = (TodoItem)BindingContext;
+                    await App.Database.SaveItemAsync(todoItem);
+                    await Navigation.PopAsync();
+                };
+             */
+        }
+
         public MyPage()
         {
             InitializeComponent();
